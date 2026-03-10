4129c63ce8a2fa304d2ce2443debd62f6b99c1ba
Shashwat Choubey <shashwatchoubey2@gmail.com>
2026-03-10 19:05:37 +0530

    Memory Added

--- agent.py
+++ agent.py
@@ -3,15 +3,17 @@ from dotenv import load_dotenv
 from tavily import TavilyClient
 from langgraph.graph import StateGraph,END,START
 from langchain_google_genai import ChatGoogleGenerativeAI
-from typing import TypedDict
+from typing import TypedDict,List
 
 load_dotenv()
 model = ChatGoogleGenerativeAI(model="gemini-3-flash-preview")
 tavily_client = TavilyClient(api_key=os.environ["TAVILY_API_KEY"])
 
+
 class AgentState(TypedDict):
     question:str
     result:str
+    history:List[str]
     
 
 def node1(state: AgentState):
@@ -19,12 +21,15 @@ def node1(state: AgentState):
     return {"result":response}
 
 def node2(state:AgentState):
+    his = "\n".join(state["history"])
+    
+
     messages = [(
         "human",
-        f"Summarize this research:{state['result']}"
+        f"Previous contect:{his} \n Summarize this research:{state['result']}+"
     )]
     output = model.invoke(messages)
-    return {"result":output.content}
+    return {"result":output.content,"history":state["history"]+[f"Q:{state['question']} A:{output.content}"]}
 
 builder = StateGraph(AgentState)
 builder.add_node("node_1",node1)
@@ -35,5 +40,6 @@ builder.add_edge("node_2",END)
 
 graph = builder.compile()
 if __name__ == "__main__":
-    test = graph.invoke({"question":"What is the current crude oil price now"})
-    print(test["result"])
+    test = graph.invoke({"question":"What is the current crude oil price now", "history":[]})
+    test2 = graph.invoke({"question":"Why did it change recently", "history":test["history"]})
+    print(test2["history"])
